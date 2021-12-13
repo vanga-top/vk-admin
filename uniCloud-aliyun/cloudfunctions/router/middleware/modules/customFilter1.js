e46945478978eeae2513d8e2c888ba4eb51fb3ae
VK云桌面 <370725567@qq.com>
2021-12-13 10:01:18 +0800

    1.8.7

--- uniCloud-aliyun/cloudfunctions/router/middleware/modules/customFilter1.js
+++ uniCloud-aliyun/cloudfunctions/router/middleware/modules/customFilter1.js
@@ -7,7 +7,7 @@ module.exports = [
 		id: "xxxx1",
 		regExp: "^xxx1/kh", // 正则匹配规则，这个是以^xxx1/kh/开头的云函数会被拦截
 		description: "这里是你过滤器1号的描述",
-		index: 210, // 由于kh（检测token）过滤器的执行顺序是200，如果需要在检测登录的过滤后执行，则需要填写 > 200 的值
+		index: 310,// 此处必须>300 因为检测用户是否登录的过滤器的index是200（sys是300，因此为了能通用，建议填大于300的值）（越小越先执行）
 		mode:"onActionExecuting", // 可选 onActionExecuting onActionExecuted
 		main: async function(event) {
 			// 这里是拦截规则，可以查数据库，最终code:0 代表通过，其他均为未通过，msg是被拦截的原因
--- uniCloud-aliyun/cloudfunctions/router/middleware/modules/returnUserInfoFilter.js
+++ uniCloud-aliyun/cloudfunctions/router/middleware/modules/returnUserInfoFilter.js
@@ -19,7 +19,7 @@ module.exports = [
 			"^client/user/pub/login(.*)"
 		],
 		description: "可用于指定哪些云函数需要返回给前端userInfo",
-		index: 210,
+		index: 310,
 		mode:"onActionExecuted",// 可选 onActionExecuting onActionExecuted
 		enable:true, // 通过设置enable=false可以关闭该中间件
 		main: async function(event, serviceRes) {
