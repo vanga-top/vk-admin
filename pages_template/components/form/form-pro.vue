078cec5ba6218fbf1b94d0522977c84aa7cf82dc
VK云桌面 <370725567@qq.com>
2023-01-03 22:10:05 +0800

    update form/form-pro.vue

--- pages_template/components/form/form-pro.vue
+++ pages_template/components/form/form-pro.vue
@@ -217,59 +217,59 @@
 									multiple:true
 								}
 							},
-              {
-                key:"cascader3", title:"云端数据级联懒加载", type:"cascader",
-                action:"admin/system/menu/sys/getCascader",
-                props:{
-                  list:"rows",
-                  value:"menu_id",
-                  label:"label",
-                  children:"children",
-                  lazy:true,
-                }
-              },
 							{
-							  key:"tree1", title:"树形选择(单选)", type:"tree-select", placeholder:"请选择学科",
-							  data:[
-							    {
-							      value:1,
-							      label:"数学",
-							      children:[
-							        { value:11,label:"奥数" },
-							        { value:12,label:"微积分" }
-							      ]
-							    },
-							    {
-							      value:2,
-							      label:"语文",
-							      children:[
-							        { value:21, label:"文言文" },
-							        { value:22, label:"古诗" }
-							      ]
-							    },
-							  ]
+								key:"cascader3", title:"云端数据级联懒加载", type:"cascader",
+								action:"admin/system/menu/sys/getCascader",
+								props:{
+									list:"rows",
+									value:"menu_id",
+									label:"label",
+									children:"children",
+									lazy:true,
+								}
 							},
 							{
-							  key:"tree2", title:"树形选择(多选)", type:"tree-select", placeholder:"请选择学科",
+								key:"tree1", title:"树形选择(单选)", type:"tree-select", placeholder:"请选择学科",
+								data:[
+									{
+										value:1,
+										label:"数学",
+										children:[
+											{ value:11,label:"奥数" },
+											{ value:12,label:"微积分" }
+										]
+									},
+									{
+										value:2,
+										label:"语文",
+										children:[
+											{ value:21, label:"文言文" },
+											{ value:22, label:"古诗" }
+										]
+									},
+								]
+							},
+							{
+								key:"tree2", title:"树形选择(多选)", type:"tree-select", placeholder:"请选择学科",
 								multiple:true, 
-							  data:[
-							    {
-							      value:1,
-							      label:"数学",
-							      children:[
-							        { value:11,label:"奥数" },
-							        { value:12,label:"微积分" }
-							      ]
-							    },
-							    {
-							      value:2,
-							      label:"语文",
-							      children:[
-							        { value:21, label:"文言文" },
-							        { value:22, label:"古诗" }
-							      ]
-							    },
-							  ]
+								data:[
+									{
+										value:1,
+										label:"数学",
+										children:[
+											{ value:11,label:"奥数" },
+											{ value:12,label:"微积分" }
+										]
+									},
+									{
+										value:2,
+										label:"语文",
+										children:[
+											{ value:21, label:"文言文" },
+											{ value:22, label:"古诗" }
+										]
+									},
+								]
 							},
 							{ key:"switch", title:"switch类型", type:"switch" },
 							{ key:"rate", title:"评分类型", type:"rate", allowHalf:false },
@@ -302,9 +302,9 @@
 								limit:6,
 								accept:".txt,.xls,.xlsx,.doc,.docx,.ppt,.pptx,.pdf"
 							},
-              { key:"image3", title:"从素材库多选图片", type:"file-select", placeholder:"请选择图片", fileType:"image", multiple:true, multipleLimit:5 },
-             	{ key:"image4", title:"从素材库单选图片", type:"file-select", placeholder:"请选择图片", fileType:"image" },
-              { key:"video1", title:"从素材库单选视频", type:"file-select", placeholder:"请选择视频", fileType:"video" },
+							{ key:"image3", title:"从素材库多选图片", type:"file-select", placeholder:"请选择图片", fileType:"image", multiple:true, multipleLimit:5 },
+							{ key:"image4", title:"从素材库单选图片", type:"file-select", placeholder:"请选择图片", fileType:"image" },
+							{ key:"video1", title:"从素材库单选视频", type:"file-select", placeholder:"请选择视频", fileType:"video" },
 							// 日期型字段
 							{ key:"", title:"日期型字段", type:"bar-title" },
 							{ key:"date", title:"date类型", type:"date", dateType:"date", tips:"可选择年月日" },
@@ -388,7 +388,7 @@
 									{ key:"text8", title:"单行文本8", type:"text" },
 								]
 							},
-						  // 对象类型
+							// 对象类型
 							{ key:"", title:"对象类型", type:"bar-title" },
 							{ key:"object1", title:"对象类型1", type:"object",
 								columns:[
@@ -404,13 +404,13 @@
 								]
 							},
 							// 多层嵌套object
-							{ key:"object3", title:"对象类型3", type:"object", 
+							{ key:"object3", title:"对象类型3", type:"object", showLabel:true,
 								columns:[
 									{ key:"a", title:"对象内属性a", type:"text" },
-									{ key:"object3_1", title:"嵌套对象类型", type:"object", 
+									{ key:"b", title:"嵌套对象b", type:"object", showLabel:true, 
 										columns:[
-											{ key:"a", title:"对象内属性a", type:"text" },
-											{ key:"b", title:"对象内属性b", type:"text" },
+											{ key:"b1", title:"嵌套对象b内属性b1", type:"text" },
+											{ key:"b2", title:"嵌套对象b内属性b2", type:"text" },
 										]
 									},
 								]
@@ -420,47 +420,47 @@
 							{ key:"editor", title:"富文本类型", type:"editor", width:"1000px" },
 							{ key:"json", title:"json类型", type:"json" },
 							{ key:"position", title:"地图位置", type:"map", width:600, height:300, defaultLocation:{ latitude: 30.224781, longitude: 120.12438 } },
-              { key:"array1", title:"数组字符串类型", type:"array<string>" },
-              { key:"array2", title:"数组数字类型", type:"array<number>" },
-              {
-                key:"array", title:"数组对象类型", type:"array<object>", itemWidth:260,
-                showAdd:true,
-                showClear:true,
-                showSort:true,
-                // 新增一行时,该行的默认值
-                defaultValue:{
-                  select1:1
-                },
-                rightBtns:['copy','delete'],
-                // 每行每个字段对应的渲染规则
-                columns:[
-                  {
-                    key:"text1", title:"昵称", type:"text",
-                    isUnique:true,
-                    rules:[
-                      { required:true, message:"该项不能为空", trigger:["change","blur"] },
-                      { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: ["change","blur"] }
-                    ]
-                  },
-                  {
-                    key:"number1", title:"数字", type:"number",
-                    rules:[
-                      { required:true, message:"该项不能为空", trigger:["change","blur"] },
-                    ]
-                  },
-                  {
-                    key:"select1", title:"select类型", type:"select",
-                    data:[
-                      { value:1, label:"选项1" },
-                      { value:2, label:"选项2" }
-                    ],
-                    rules:[
-                      { required:true, message:"该项不能为空", trigger:["change","blur"] },
-                    ]
-                  },
-                  { key:"switch", title:"switch类型", type:"switch", width:160 },
-                ]
-              },
+							{ key:"array1", title:"数组字符串类型", type:"array<string>" },
+							{ key:"array2", title:"数组数字类型", type:"array<number>" },
+							{
+								key:"array", title:"数组对象类型", type:"array<object>", itemWidth:260,
+								showAdd:true,
+								showClear:true,
+								showSort:true,
+								// 新增一行时,该行的默认值
+								defaultValue:{
+									select1:1
+								},
+								rightBtns:['copy','delete'],
+								// 每行每个字段对应的渲染规则
+								columns:[
+									{
+										key:"text1", title:"昵称", type:"text",
+										isUnique:true,
+										rules:[
+											{ required:true, message:"该项不能为空", trigger:["change","blur"] },
+											{ min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: ["change","blur"] }
+										]
+									},
+									{
+										key:"number1", title:"数字", type:"number",
+										rules:[
+											{ required:true, message:"该项不能为空", trigger:["change","blur"] },
+										]
+									},
+									{
+										key:"select1", title:"select类型", type:"select",
+										data:[
+											{ value:1, label:"选项1" },
+											{ value:2, label:"选项2" }
+										],
+										rules:[
+											{ required:true, message:"该项不能为空", trigger:["change","blur"] },
+										]
+									},
+									{ key:"switch", title:"switch类型", type:"switch", width:160 },
+								]
+							},
 							{ key:"goods_tags", title:"商品标签", type:"tag" },
 						],
 						// 表单验证规则
